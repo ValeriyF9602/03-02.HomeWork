e55e826f6938f251a334862901bf77e5b83842df
Valeriy Fateev <Valerka9602@mail.ru>
2023-02-18 19:04:53 +0800

    Решение задачи 15

--- ex15/Program.cs
+++ ex15/Program.cs
@@ -4,3 +4,20 @@
 // 7 -> да
 // 1 -> нет
 
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else
+    {
+        Console.WriteLine("Будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
